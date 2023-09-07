2f2fadbf9363ca1532a8b2f632cc22446bcf329b
SJ Graves <sjgraves@gmail.com>
2023-09-07 12:38:18 -0500

    reorg into folders

--- AAM-PKV/Changes.md
+++ AAM-PKV/Changes.md
@@ -9,3 +9,5 @@ Tags:
 ## 2023-09-05
 + Started investigating exercises.
 	+ Started writing a first-day exercise which will help newbies get started with Python.
+## 2023-09-07
++ After substantial changes to exercises, I realized I was largely plagiarizing the _Learn Python 3 the Hard Way_ book and needed to not do that.
--- AAM-PKV/aam-python/project-01/prj01_02.py
+++ AAM-PKV/aam-python/project-01/prj01_02.py
@@ -3,5 +3,5 @@ print("Hello world!")
 print("Hello yourself")
 print('Type this exactly the way that I have typed it')
 print("Don't change anything, even if you feel 'smart.'")
-print("""Sometimes things don't do what you would expect, and\n
-\tthat's okay""")
+print("""\tSometimes things don't do what you would expect, and\n
+         that's\t\tokay""")
